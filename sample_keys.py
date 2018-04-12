9a51f50554e296efd445760de31e8a1c543ed86e
Victor <ngenovictor321@gmail.com>
2018-04-12 22:56:39 +0300

    add sample key

---
+++ sample_keys.py
@@ -0,0 +1,3 @@
+consumer_key = "consumer_key"  # get from your developer account
+consumer_secret = "consumer_secret"  # get from your developer account
+phone_number = "254721234567"  # your phone number
